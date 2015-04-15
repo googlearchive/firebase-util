2da72b78d91f4079b414cda1f3aaa48de5dcf3df
katowulf <katowulf@gmail.com>
2015-04-14 18:16:46 -0700

    Bump Firebase and fbutil versions.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
    var app = angular.module('app', ['ui.router', 'ui.bootstrap', 'ngSanitize', 'firebase']);
 
   app.constant('VERSION_FIREBASE', '2.2.3');
-  app.constant('VERSION_FBUTIL', '0.2.3');
+  app.constant('VERSION_FBUTIL', '0.2.4');
 
   // later if we have more libs we'll want to switch to this layout
    app.config(['$stateProvider', '$urlRouterProvider',
